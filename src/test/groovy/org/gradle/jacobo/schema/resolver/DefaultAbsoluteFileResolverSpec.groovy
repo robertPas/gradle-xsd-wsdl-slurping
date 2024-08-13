f896cd276dc6344238c493d912e76a5c12792b21
Robert Paschek <robert.paschek@comm-unity.at>
2024-08-13 20:27:32 +0200

    tests corrected
    I guess before they were only working on Linux

--- src/test/groovy/org/gradle/jacobo/schema/resolver/DefaultAbsoluteFileResolverSpec.groovy
+++ src/test/groovy/org/gradle/jacobo/schema/resolver/DefaultAbsoluteFileResolverSpec.groovy
@@ -15,10 +15,10 @@ class DefaultAbsoluteFileResolverSpec extends Specification {
 
   where:
   parent                    | relativeLocation                  || result
-  new File("/schema/group") | "../otherGroup/subGroup/type.xsd" || new File("/schema/otherGroup/subGroup/type.xsd")
-  new File("/wsdl/concrete")| "../abstract/abstract.wsdl"       || new File("/wsdl/abstract/abstract.wsdl")
-  new File("/wsdl/concrete")| "../../equal.xsd"                 || new File("/equal.xsd")
-  new File("/someDir")      | "./subDir/sub.xsd"                || new File("/someDir/subDir/sub.xsd")
-  new File("/someDir")      | "subDir/other-sub.xsd"            || new File("/someDir/subDir/other-sub.xsd")
+  new File("/schema/group") | "../otherGroup/subGroup/type.xsd" || new File(new File("/schema/otherGroup/subGroup/type.xsd").canonicalPath)
+  new File("/wsdl/concrete")| "../abstract/abstract.wsdl"       || new File(new File("/wsdl/abstract/abstract.wsdl").canonicalPath)
+  new File("/wsdl/concrete")| "../../equal.xsd"                 || new File(new File("/equal.xsd").canonicalPath)
+  new File("/someDir")      | "./subDir/sub.xsd"                || new File(new File("/someDir/subDir/sub.xsd").canonicalPath)
+  new File("/someDir")      | "subDir/other-sub.xsd"            || new File(new File("/someDir/subDir/other-sub.xsd").canonicalPath)
   }
 }
